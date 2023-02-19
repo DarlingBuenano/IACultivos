c75965654334d0942f1a7e75d5bc4ef5ff16bbae
DarlingBuenano <dwverling@gmail.com>
2023-02-19 22:32:45 +0100

    configuraciones del servidor de la uteq

--- IACultivos/settings.py
+++ IACultivos/settings.py
@@ -8,9 +8,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-iiu3qf(3syr+#eg_#c6tw2q@3n*e%ptlawp@-%)&#r)i$-jb@-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['machlear.uteq.edu.ec', '127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -101,7 +101,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 #STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
